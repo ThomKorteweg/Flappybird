1ed02746f39a8a3d2cfef6af8c4815fd38f5bf38
Tijmen Schouten <74767479+tijmenhero@users.noreply.github.com>
2021-10-01 08:55:23 +0000

    ruwe collision

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ let flappy;
 let img;
 var jumpSound;
 let pijp
+let tijmenkast
 
 function preload(){
   img = loadImage('background.jpg');
-  flappy = loadImage('flappy.png');
+  flappy = loadImage('flappy bird.png');
   pijp = loadImage('pijp.png');
   jumpSound = loadSound('wing.mp3');
+  tijmenkast = loadImage('tijmenkast.jpg')
 }
 
 
@@ -19,7 +21,7 @@ class Ball {
     this.w = w;
     this.h = h;
     this.vy = vy;
-    this.ay = 0.3;
+    this.ay = 0.4;
   }
   drawBall() {
     image(flappy, this.x, this.y, this.w, this.h);
@@ -32,7 +34,7 @@ class Ball {
       this.ay = 0;
     }
     else {
-    this.ay = 0.3;
+    this.ay = 0.4;
     }
     if (this.y < 0) {
       this.y = 0
@@ -47,19 +49,23 @@ class Pijp {
     this.y = y;
     this.w = w;
     this.h = h;  
+    this.c = "green";
   }
   drawPijp() { 
-    fill('green');
+    fill(this.c);
+    //image(pijp, this.x, this.y, this.w, this.h, this.color)
     rect(this.x, this.y, this.w, this.h, this.color);
     this.x -= 4;
   }
 
-  checkCollision(){
-    if(ball1.x + ball1.w/2 > this.x){
-      console.log('bumped');
+  isColliding(){
+    if(ball1.x > this.x && this.x + this.w < ball1.x){
+      this.c = "red";
+    }
+    else{
+    this.c = "green";
     }
   }
-
 }
 
 var ball1, rect1, rect2;
@@ -68,39 +74,41 @@ var pipes = [];
 function setup() {
   createCanvas(600, 400); 
   ball1 = new Ball(250, 50, 40, 30, 3); 
-}   
+}
+
 
 function draw() {
   background(110, 0, 32);  
-   image(img,0,0,600,520); 
-
+   image(img,0,0,600,520);
+   
   textSize(30);
   fill('white');
   text("0",250, 50);
 
-  if(frameCount % 80 == 0 ){   
-    let randomTopHeight = random(height / 2);
-    console.log(randomTopHeight);
+  if(frameCount % 70 == 0 ){   
+    let randomTopHeight = random(height / 2);  
+  
     rect1 = new Pijp(700, 0, 50, randomTopHeight, 3);
     rect2 = new Pijp(700, randomTopHeight + 120, 50, height, 3);
     pipes.push(rect1, rect2);
 
+    if(pipes.length > 6){
+      pipes.splice(0,2);
+    }
+    
   }
 
-  ball1.drawBall();
-  pipes.forEach(p => p.drawPijp());
+  ball1.drawBall(); 
+  pipes.forEach((p) => {
+    p.drawPijp();
+    p.isColliding();
+
+});
 }
-
-
 
 function keyPressed() {
   if (keyCode == 32) {
-    ball1.vy = -6;
+    ball1.vy = -7;
     jumpSound.play()
   }
 }
-
-
-
-
-
